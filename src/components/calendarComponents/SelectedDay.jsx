c749aea8ea6e4c900d5bdd0092283533f9a2c0d6
Tobleroo <tobias.jaks@gmail.com>
2023-08-08 11:24:49 +0200

    design and starting on calendar saving

--- src/components/calendarComponents/SelectedDay.jsx
+++ src/components/calendarComponents/SelectedDay.jsx
@@ -4,8 +4,32 @@ import "../../styles/SelectedDay.css";
 
 function SelectedDay() {
 
+function switchTaskEventInput(inputWanted){
 
+    if(inputWanted === "event"){
+        document.querySelector(".add-event").style.display = "block";
+        document.querySelector(".add-task").style.display = "none";
+        document.querySelector("#eventBtn").style.backgroundColor = "red";
+        document.querySelector("#taskBtn").style.backgroundColor = "white";
+    } else if(inputWanted === "task"){
+        document.querySelector(".add-event").style.display = "none";
+        document.querySelector(".add-task").style.display = "block";
+        document.querySelector("#eventBtn").style.backgroundColor = "white";
+        document.querySelector("#taskBtn").style.backgroundColor = "red";
+    }
+}
 
+function addTaskToCalendar(){
+
+}
+
+function addEventToCalendar(){
+
+}
+
+function saveCalendarToServer(){
+
+}
 
 
 return (
@@ -14,14 +38,54 @@ return (
             <h4>selected date</h4>
         </div>
         <div className="task-box">
-            <h4>tasks:</h4>
+            <div className="details-box">
+                <h4>name</h4>
+                <h4>time</h4>
+                <h4>completed</h4>
+            </div>
+            <div className="active-tasks">
+                <h4 className="task-title">clean house</h4>
+                <p className="task-time">20 minutes</p>
+                <button>complete task</button>
+            </div>
         </div>
         <div className="event-box">
-            <h4>events:</h4>
+            <div className="details-box">
+                <h4>name</h4>
+                <h4>time</h4>
+            </div>
+            <div className="active-events">
+                <h4 className="event-title">go to show</h4>
+                <h4 className="event-time">19:00</h4>
+            </div>
+        </div>
+        <div className="type-add-box">
+            <button type="button" className="type-Btn" id="eventBtn" onClick={() => {
+                switchTaskEventInput("event");
+            }}>event</button>
+            <button type="button" className="type-Btn" id="taskBtn" onClick={() => {
+                switchTaskEventInput("task");
+            }}>task</button>
         </div>
         <div className="add-box">
-            <button>+</button>
+            <div className="selected-type-box">
+                <div className="add-event">
+                    <label htmlFor="addTitleId">title:</label>
+                    <input type="text" name="event-title" id="addTitleId" />
+                    <label htmlFor="addDescriptionId">description:</label>
+                    <input type="text" name="event-description" id="addDescriptionId" />
+                    <label htmlFor="addTimeId">time:</label>
+                    <input type="time" name="event-time" id="addTimeId" />
+                </div>
+                <div className="add-task">
+                    <label htmlFor="addTitleId">title:</label>
+                    <input type="text" name="task-title" id="addTitleId" />
+                    <label htmlFor="addTimeId">description:</label>
+                    <input type="number" name="task-time" id="addTimeId" />
+                </div>
+            </div>
         </div>
+        <button>add to calendar</button>
 
     </div>
 );
